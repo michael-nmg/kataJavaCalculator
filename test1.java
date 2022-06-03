6d4a40b4a9c906cb5bb6ec30a911a914ebae0d88
michael-nmg <michael.nmg@gmail.com>
2022-06-03 15:42:39 +0300

    Lambda with interface

--- test1.java
+++ test1.java
@@ -8,102 +8,56 @@ import java.util.ArrayList;
 //import java.util.function.*;
 //import java.util.List;
 //import java.util.stream.*;
-import java.util.HashMap;
 //import java.util.concurrent.Future;
+import java.util.HashMap;
 
 ////////////////////////////////////////////////////////////////////////////////////////
 
 public class Main {
-    public void main (String[] args) {
-          //String reg = "\\w+|([+-/*])";
-          //Pattern pattern = Pattern.compile("\\w+|([+-/*])");
-          Integer a = new Integer(0);
-          Integer b = new Integer(0);
-          HashMap<String, Runnable> hash = new HashMap<>();
-          hash.put("+", () -> System.out.println(a + b));
-          hash.put("*", () -> System.out.println(a * b));
-          hash.put("/", () -> System.out.println(a / b));
-          hash.put("-", () -> System.out.println(a - b));
-
-          String scan = new Scanner(System.in).nextLine().trim();
-          Matcher matcher = Pattern.compile("\\w+|([+-/*])").matcher(scan);
-          ArrayList<String> list = new ArrayList<String>();
-          String[] arr = new String[3];
-          for (int i = 0; matcher.find(); i++) {
-             arr[i] = matcher.group().trim();
-          }
-          int a = Integer.parseInt(arr[0]);
-          int b = Integer.parseInt(arr[2]);
-          hash.get(arr[1]).call();
+    static interface calcInterface {
+        int calc(int x, int y);
     }
-}
 
-////////////////////////////////////////////////////////////////////////////////////////
-
-public class Main {
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        String now = scan.nextLine().trim();
-        String x = "Hello";
-        char[] arr = {};
+      while (true) {
+        //System.out.println();
 
-        String result = MessageFormat.format(
-          "Your text: {0};\nHis length: {1};\nExample: {2}\nExample: {3}",
-          now, now.length(), now.compareTo(x), now.toCharArray(), now.indexOf('l')
-        );
+              String scan = new Scanner(System.in).nextLine().trim();
+          Matcher matcher = Pattern.compile("\\w+|([+-/*])").matcher(scan);
 
-        System.out.println(result);
-    }
-}
+          String[] arr = new String[3];
+          //try catch dlinna massiva
+          for (int i = 0; matcher.find(); i++) {
+             arr[i] = matcher.group();//.trim();
+          }
+      //MessageFormat.format("Your text: {0};\nHis length: {1};\nExample: {2}\nExample: {3}",
+      System.out.println(calcLamb(arr[1], Integer.parseInt(arr[0]), Integer.parseInt(arr[2])));
 
-////////////////////////////////////////////////////////////////////////////////////////
-
-public class Result {
-    int plus (int[] args) {
-        return args[0] + args[1];
+      }
     }
 
-    int minus (int[] args) {
-        return args[0] - args[1];
-    }
+    public static int calcLamb(String oper, int x, int y) {
+        calcInterface add = (a, b) -> a + b;
+        calcInterface sub = (a, b) -> a - b;
+        calcInterface mul = (a, b) -> a * b;
+        calcInterface div = (a, b) -> a / b;
 
-    int div (int[] args) {
-        return args[0] / args[1];
-    }
-
-    int mult (int[] args) {
-        return args[0] * args[1];
-    }
-}
-
-int[] arr = {1,2,34,45};
-for (int i = 0; i < arr.length; i++) {
-  System.out.println(arr[i]);
-}
-
-///////////////////////////////////////////////////////////////////////////////////
-
-function romanNumerals(number) {
-    let data = (number + '').split('').reverse();
-    let rome = [['I', 'V'], ['X', 'L'], ['C', 'D'], ['M']];
-    function f(i, k) {
-        let res = '';
-        i = i * 1;
-        if (i === 9) {
-            res = rome[k][0] + rome[k + 1][0];
-        } else if (i >= 5) {
-            res = i === 5 ? rome[k][1] : rome[k][1] + rome[k][0].repeat(i - 5);
-        } else if (i <= 4) {
-            res = i === 4 ? rome[k].join('') : rome[k][0].repeat(i);
+        switch(oper) {
+            case "+":
+                return add.calc(x, y);
+            case "-":
+                return sub.calc(x, y);
+            case "*":
+                return mul.calc(x, y);
+            case "/":
+              return div.calc (x, y);
         }
-        return res;
+        return 0;//"Bad operator";//try catch
     }
-    return data.map(f).reverse().join('');
 }
 
 ///////////////////////////////////////////////////////////////////////////////////
 
-import java.io.*;
 public class Test {
 
    public static void main(String args[]) {
