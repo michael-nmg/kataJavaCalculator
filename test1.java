921f43304cee591d354d501792a0705ec658cced
michael-nmg <michael.nmg@gmail.com>
2022-06-03 22:23:58 +0300

    Romule numbers Познали стримы, но стримы не спасают от поиска индексов.

--- test1.java
+++ test1.java
@@ -1,15 +1,16 @@
 import java.io.*;
 import java.util.Scanner;
+import java.util.stream.*;
 import java.util.regex.*;
 import java.text.MessageFormat;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.ArrayList;
 //import java.util.regex.Pattern;
 //import java.util.function.*;
 //import java.util.List;
 //import java.util.stream.*;
 //import java.util.concurrent.Future;
-import java.util.HashMap;
 
 ////////////////////////////////////////////////////////////////////////////////////////
 
@@ -19,23 +20,20 @@ public class Main {
     }
 
     public static void main(String[] args) {
-      while (true) {
-        //System.out.println();
-
-              String scan = new Scanner(System.in).nextLine().trim();
-          Matcher matcher = Pattern.compile("\\w+|([+-/*])").matcher(scan);
-
-          String[] arr = new String[3];
-          //try catch dlinna massiva
-          for (int i = 0; matcher.find(); i++) {
-             arr[i] = matcher.group();//.trim();
-          }
-      //MessageFormat.format("Your text: {0};\nHis length: {1};\nExample: {2}\nExample: {3}",
-      System.out.println(calcLamb(arr[1], Integer.parseInt(arr[0]), Integer.parseInt(arr[2])));
-
-      }
+        //System.out.println(romToInt("MMDCLXXXIX"));
+        while (true) {
+            //System.out.println();
+            String scan = new Scanner(System.in).nextLine().trim();
+            Matcher matcher = Pattern.compile("\\w+|([+-/*])").matcher(scan);
+            String[] arr = new String[3];
+            //try catch dlinna massiva
+            for (int i = 0; matcher.find(); i++) {
+                arr[i] = matcher.group();//.trim();
+            }
+            //MessageFormat.format("Your text: {0};\nHis length: {1};\nExample: {2}\nExample: {3}",
+            System.out.println(calcLamb(arr[1], Integer.parseInt(arr[0]), Integer.parseInt(arr[2])));
+        }
     }
-
     public static int calcLamb(String oper, int x, int y) {
         calcInterface add = (a, b) -> a + b;
         calcInterface sub = (a, b) -> a - b;
@@ -50,9 +48,30 @@ public class Main {
             case "*":
                 return mul.calc(x, y);
             case "/":
-              return div.calc (x, y);
-        }
+                return div.calc (x, y);
+            }
         return 0;//"Bad operator";//try catch
+    }
+
+    public static int romToInt (String num) {
+        HashMap<String, Integer> romChar = new HashMap<>();
+        romChar.put("M", 1000);
+        romChar.put("D", 500);
+        romChar.put("C", 100);
+        romChar.put("L", 50);
+        romChar.put("X", 10);
+        romChar.put("V", 5);
+        romChar.put("I", 1);
+
+        Integer[] array = Arrays.stream(
+            num.trim().toUpperCase().replaceAll("\\s*", "").split(""))
+            .map(x -> romChar.get(x))
+            .toArray(Integer[]::new);
+
+        return IntStream.range(0, array.length - 1).reduce(0, (acc, i) -> {
+            acc += (array[i] >= array[i + 1] ? array[i] : -array[i]);
+            return acc;
+        }) + array[array.length - 1];
     }
 }
 
