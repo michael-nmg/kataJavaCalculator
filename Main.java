150b3f1619ee95b54dbdfb57c7ab92b444166777
michael-nmg <michael.nmg@gmail.com>
2022-06-05 19:54:01 +0300

    final commit, betta

--- Main.java
+++ Main.java
@@ -1,70 +1,64 @@
 import java.io.*;
 import java.util.Scanner;
-import java.util.stream.*;
 import java.util.regex.*;
-import java.text.MessageFormat;
-import java.util.Arrays;
 import java.util.HashMap;
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
-//import java.util.Exeption;
-//import java.util.regex.Pattern;
-//import java.util.function.*;
-//import java.util.stream.*;
-//import java.util.concurrent.Future;
-
-///////////////////////////////////////////////////////////////
+import java.util.ArrayList;
+import java.util.stream.*;
 
 public class Main {
     static interface calcInterface {
         int calcOp(int x, int y);
     }
 
-    public static String calc(String input) throws CalcExceptions {
-        Matcher matcher = Pattern.compile("\\w+|([+-/*])").matcher(input);
+    public static String calc(String input) throws CalcExceptions,
+    ArrayIndexOutOfBoundsException {
+        Matcher matcher = Pattern.compile("\\d*[.,]\\d*|\\w+|([+-/*!;%:?@#$%^&])").matcher(input);
         String[] arr = new String[3];
+        boolean calcError = false;
 
-      try {
-          for (int i = 0; matcher.find(); i++) {
-            arr[i] = matcher.group();//.trim();
-          }
-        } catch (ArrayIndexOutOfBoundsException excep) {
-          System.out.println("\n\n\tOnly binary operations are possible.\n\n");
-          //break;
+        try {
+            for (int i = 0; matcher.find(); i++) {
+                arr[i] = matcher.group();
+            }
+        } catch (ArrayIndexOutOfBoundsException exec) {
+            calcError = true;
         }
 
+        if (calcError) {
+            throw new CalcExceptions("\n\n\t Сalculator only supports binary operations \n\n");
+        };
+        boolean rom = (isRome(arr[0]) && isRome(arr[2])) ? true : false;
+        boolean dig = (isDig(arr[0]) && isDig(arr[2])) ? true : false;
 
+        if ( !(rom ^ dig) ) {
+            throw new CalcExceptions("\n\n\tDifferent number systems are used.\n\n");
+        } else if (!isOper(arr[1])) {
+            throw new CalcExceptions("\n\n\tThe operation is not defined.\n\tPlease, use the available: +, -, *, /\n\n");
+        }
 
-      int var1 = Integer.parseInt(arr[0]);
-      int var2 = Integer.parseInt(arr[2]);
+        int var1 = rom ? romToInt(arr[0]) : Integer.parseInt(arr[0]);
+        int var2 = rom ? romToInt(arr[2]) : Integer.parseInt(arr[2]);
 
-      if (!(arr.length == 3)) {
-        throw new CalcExceptions("");
-      } else if (!isOper(arr[1])) {
-        throw new CalcExceptions("\n\n\tThe operation is not defined.\n\tPlease, use the available: +, -, *, /\n\n");
-      } else if (isDig(arr[0]) ^ isDig(arr[2])) {
-        throw new CalcExceptions("\n\n\tDifferent number systems are used.\n\n");
-      } else if (var1 < 1 && var1 > 10 || var2 < 1 && var2 > 10){
+        if ((var1 < 1 || var1 > 10) || (var2 < 1 || var2 > 10)) {
+            throw new CalcExceptions("\n\n\tThe variable exceeded the allowed values: [1, 10].\n\n");
+        };
 
-      };
+        int result = calcLamb(arr[1], var1, var2);
+        if (rom && result < 1) {
+            throw new CalcExceptions("\n\n\tRoman numerals are integers and can't be less than one.\n\n");
+        };
 
-      //isRome + romeFactorBoolean
-
-
-      int result = calcLamb(arr[1], var1, var2));
-
-      return result + "";
+        return (rom ? intToRom(result): result) + "";
     }
 
     public static void main(String[] args) throws CalcExceptions {
-        //System.out.println(MessageFormat.format("\n\n\tCalculator for binary operations.\n\tSupported operations: *, +, /, -.\n\tFor exit press 'CTRL+C'\n\n {0};\nHis length: {1};\nExample: {2}\nExample: {3}",)
-        //System.out.println(romToInt("MMDCLXXXIX"));
+        System.out.println("\n\n\tCalculator for binary operations.\n\tSupported operations: *, +, /, -.\n\tFor exit press CTRL+C\n\n");
+
         while (true) {
-        String scan = new Scanner(System.in).nextLine().trim();
-
-        System.out.println(calc(scan));
-
-
+            String scan = new Scanner(System.in).nextLine().trim();
+            System.out.println(calc(scan));
         }
     }
 
@@ -83,8 +77,8 @@ public class Main {
                 return mul.calcOp(x, y);
             case "/":
                 return div.calcOp (x, y);
-            }
-        return 0;//"Bad operator";//try catch
+        }
+        return 0;
     }
 
     public static int romToInt (String num) {
@@ -127,9 +121,31 @@ public class Main {
         String[] numAr = num.toUpperCase().split("");
         boolean result = true;
         for (int i = 0; i < numAr.length; i++) {
-          result = result && charRom.contains(numAr[i]) ? true : false;
+            result = result && charRom.contains(numAr[i]) ? true : false;
         }
         return result;
     }
 
+    private static String intToRom (Integer num) {
+        String[] blank = new StringBuilder(num + "").reverse().toString().split("");
+        String result = "";
+        for (int i = 0; i < blank.length; i++) {
+            result = funcForintToRom(blank[i], i) + result;
+        }
+        return result;
+    }
+
+    private static String funcForintToRom (String elem, Integer index) {
+        String[][] romuls = new String[][]{{"I", "V"}, {"X", "L"}, {"C", "D"}, {"M"}};
+        String result = "";
+        int e = Integer.parseInt(elem);
+        if (e == 9) {
+            result = romuls[index][0] + romuls[index + 1][0];
+        } else if (e >= 5) {
+            result = e == 5 ? romuls[index][1] : romuls[index][1] + romuls[index][0].repeat(e - 5);
+        } else if (e <= 4) {
+            result = e == 4 ? String.join("", romuls[index]) : romuls[index][0].repeat(e);
+        }
+        return result;
+    }
 }
