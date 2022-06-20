0c81f54ac16ff300832e3edf9d67205c92e6a747
michael-nmg <michael.nmg@gmail.com>
2022-06-20 04:57:13 +0300

    refactoring: calc function

--- Main.java
+++ Main.java
@@ -1,58 +1,49 @@
 import java.io.*;
-import java.util.Scanner;
+import java.util.*;
 import java.util.regex.*;
-import java.util.HashMap;
-import java.util.Arrays;
-import java.util.List;
-import java.util.ArrayList;
 import java.util.stream.*;
-import java.util.function.IntBinaryOperator;
+import java.util.function.*;
 
 public class Main {
-
     public static String calc(String input) throws CalcExceptions, ArrayIndexOutOfBoundsException {
-        Matcher matcher = Pattern.compile("\\d*[.,]\\d*|\\w+|([+-/*!;%:?@#$%^&])").matcher(input);
-        String[] arr = new String[3];
-        boolean calcError = false;
+        String blanc = input.replace(" ", "");
+        String[] operArr = new String[] {"/", "*", "-", "+"};
 
-        try {
-            for (int i = 0; matcher.find(); i++) {
-                arr[i] = matcher.group();
-            }
-        } catch (ArrayIndexOutOfBoundsException exec) {
-            calcError = true;
+        if (blanc.isEmpty()) {
+            throw new CalcExceptions("\n\n\tString is empty. Try again.\n\n");
         }
 
-        if (calcError) {
-            throw new CalcExceptions("\n\n\t Сalculator only supports binary operations \n\n");
-        };
-        boolean rom = (isRome(arr[0]) && isRome(arr[2])) ? true : false;
-        boolean dig = (isDig(arr[0]) && isDig(arr[2])) ? true : false;
+        String operation = isEntryOper(blanc, operArr);
+        if (operation.isEmpty() || !isOperBinary(blanc, operation)) {
+            throw new CalcExceptions("\n\n\tThe operation is not defined.\n\tСalculator only supports binary operations.\n\n");
+        }
 
-        if ( !(rom ^ dig) ) {
+        String[] varrs = blanc.split("\\" + operation);
+        boolean romuls = isRome(varrs[0]) && isRome(varrs[1]);
+        boolean digits = isDigital(varrs[0]) && isDigital(varrs[1]);
+
+        if ( !(romuls ^ digits) ) {
             throw new CalcExceptions("\n\n\tDifferent number systems are used.\n\n");
-        } else if (!isOper(arr[1])) {
-            throw new CalcExceptions("\n\n\tThe operation is not defined.\n\tPlease, use the available: +, -, *, /\n\n");
         }
 
-        int var1 = rom ? romToInt(arr[0]) : Integer.parseInt(arr[0]);
-        int var2 = rom ? romToInt(arr[2]) : Integer.parseInt(arr[2]);
+        int var1 = romuls ? romToInt(varrs[0]) : Integer.parseInt(varrs[0]);
+        int var2 = romuls ? romToInt(varrs[1]) : Integer.parseInt(varrs[1]);
 
         if ((var1 < 1 || var1 > 10) || (var2 < 1 || var2 > 10)) {
             throw new CalcExceptions("\n\n\tThe variable exceeded the allowed values: [1, 10].\n\n");
-        };
+        }
 
-        int result = calcLamb(arr[1], var1, var2);
-        if (rom && result < 1) {
+        int result = calcLamb(operation, var1, var2);
+
+        if (romuls && result < 1) {
             throw new CalcExceptions("\n\n\tRoman numerals are integers and can't be less than one.\n\n");
-        };
+        }
 
-        return (rom ? intToRom(result): result) + "";
+        return (romuls ? intToRom(result): result) + "";
     }
 
     public static void main(String[] args) throws CalcExceptions {
         System.out.println("\n\n\tCalculator for binary operations.\n\tSupported operations: *, +, /, -.\n\tFor exit press CTRL+C\n\n");
-
         while (true) {
             String scan = new Scanner(System.in).nextLine().trim();
             System.out.println(calc(scan));
@@ -60,57 +51,54 @@ public class Main {
     }
 
     public static int calcLamb(String oper, int x, int y) {
-      HashMap<String, IntBinaryOperator> hh = new HashMap<>();
-      hh.put("+", (int a,int b) -> a + b);
-      hh.put("-", (int a,int b) -> a - b);
-      hh.put("*", (int a,int b) -> a * b);
-      hh.put("/", (int a,int b) -> a / b);
-      return hh.get(oper).applyAsInt(x, y);
+        HashMap<String, IntBinaryOperator> hh = new HashMap<>() {{
+            put("+", (int a,int b) -> a + b);
+            put("-", (int a,int b) -> a - b);
+            put("*", (int a,int b) -> a * b);
+            put("/", (int a,int b) -> a / b);
+        }};
+        return hh.get(oper).applyAsInt(x, y);
     }
 
-    public static int romToInt (String num) {
-        HashMap<String, Integer> romChar = new HashMap<>();
-        romChar.put("M", 1000);
-        romChar.put("D", 500);
-        romChar.put("C", 100);
-        romChar.put("L", 50);
-        romChar.put("X", 10);
-        romChar.put("V", 5);
-        romChar.put("I", 1);
+    private static String isEntryOper (String str, String[] opArr) {
+        String[] arr = Stream.of(opArr).filter(i -> str.contains(i)).toArray(String[]::new);
+        return arr.length == 1 ? arr[0] : "";
+    }
 
-        Integer[] array = Arrays.stream(
-            num.trim().toUpperCase().replaceAll("\\s*", "").split(""))
-            .map(x -> romChar.get(x))
-            .toArray(Integer[]::new);
+    private static boolean isOperBinary (String str, String oper) {
+        int ind = str.indexOf(oper);
+        int indL = str.lastIndexOf(oper);
+        return !oper.isEmpty() && ind != -1 && ind == indL ? true : false;
+    }
 
+    private static boolean isRome (String number) {
+        String charRom = "MDCLXVI";
+        String[] numArr = number.toUpperCase().split("");
+        return Stream.of(numArr).filter(i -> charRom.contains(i)).count() == number.length();
+    }
+
+    private static boolean isDigital (String number) {
+        String[] arr = number.split("");
+        return Stream.of(arr).filter(i -> Character.isDigit(i.charAt(0))).count() == number.length();
+    }
+
+    private static Integer romToInt (String num) {
+        HashMap<String, Integer> romChar = new HashMap<>() {{
+            put("M", 1000);
+            put("D", 500);
+            put("C", 100);
+            put("L", 50);
+            put("X", 10);
+            put("V", 5);
+            put("I", 1);
+        }};
+
+        Integer[] array = Stream.of(num.toUpperCase().split(""))
+            .map(x -> romChar.get(x)).toArray(Integer[]::new);
         return IntStream.range(0, array.length - 1).reduce(0, (acc, i) -> {
-            acc += (array[i] >= array[i + 1] ? array[i] : -array[i]);
+            acc += array[i] >= array[i + 1] ? array[i] : -array[i];
             return acc;
         }) + array[array.length - 1];
-    }
-
-    private static boolean isDig (String number) {
-        char[] charArr = number.toCharArray();
-        return IntStream.range(0, charArr.length).reduce(1, (acc, i) -> {
-            acc = Character.isDigit(charArr[i]) && acc == 1 ? 1 : 0;
-            return acc;
-        }) == 1 ? true : false;
-    }
-
-    private static boolean isOper (String oper) {
-        String[] operAr = new String[]{"+", "-", "*", "/"};
-        List opList = new ArrayList<>(Arrays.asList(operAr));
-        return opList.contains(oper);
-    }
-
-    private static boolean isRome (String num) {
-        String charRom = "MDCLXVI";
-        String[] numAr = num.toUpperCase().split("");
-        boolean result = true;
-        for (int i = 0; i < numAr.length; i++) {
-            result = result && charRom.contains(numAr[i]) ? true : false;
-        }
-        return result;
     }
 
     private static String intToRom (Integer num) {
